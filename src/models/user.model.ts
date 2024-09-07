bacd2c976bc75e221b1b4b4179cc98190fd421a6
YUapisit <88958116+Apisit250aps@users.noreply.github.com>
2024-09-07 22:02:29 +0700

    user model

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -3,33 +3,24 @@ import mongoose, { Document, Schema } from "mongoose"
 export interface IUser extends Document {
   username: string
   password: string
-  email: string
   createdAt?: Date
 }
 
-const UserSchema: Schema<IUser> = new Schema({
-  username: {
-    type: String,
-    required: true,
-    trim: true,
-    unique: true
+const UserSchema: Schema<IUser> = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true
+    }
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-    default: null
-  },
-  password: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
-  }
-})
+  { timestamps: true }
+)
 
 export const User = mongoose.model<IUser>("User", UserSchema)
