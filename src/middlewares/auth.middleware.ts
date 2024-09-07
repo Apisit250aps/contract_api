b392e9f24bf86813322acd211159c2ac6c5c88bd
YUapisit <88958116+Apisit250aps@users.noreply.github.com>
2024-09-07 22:56:58 +0700

    optimize auth middleware

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -2,10 +2,12 @@ import { Response, NextFunction } from "express"
 import { verifyToken } from "../utils/jwt"
 import { IUser } from "../models/user.model"
 import User from "../models/user.model"
+import { TokenExpiredError, JsonWebTokenError } from "jsonwebtoken"
+
 declare global {
   namespace Express {
     interface Request {
-      user?: IUser // Add optional user property
+      user?: IUser
       headers: {
         authorization?: string
       }
@@ -20,23 +22,33 @@ export const authenticateJWT = async (
 ) => {
   const authHeader = req.headers.authorization
 
-  if (authHeader) {
-    const token = authHeader.split(" ")[1] // Assuming the token is in the format "Bearer <token>"
+  if (!authHeader) {
+    return res.status(401).json({ error: "Authorization header is missing" })
+  }
 
-    try {
-      const decoded = verifyToken(token) as IUser // Ensure that decoded is of type IUser
-      const user = await User.findById(decoded.id) //
+  const tokenParts = authHeader.split(" ")
+  if (tokenParts.length !== 2 || tokenParts[0] !== "Bearer") {
+    return res.status(401).json({ error: "Invalid token format" })
+  }
 
-      if (!user) {
-        return res.status(404).json({ error: "User not found" })
-      }
+  const token = tokenParts[1]
 
-      req.user = user // Save user information to request object
-      next()
-    } catch (error) {
-      return res.status(403).json({ error: "Forbidden" })
+  try {
+    const decoded = verifyToken(token) as IUser
+    const user = await User.findById(decoded.id).select("-password")
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found" })
     }
-  } else {
-    return res.status(401).json({ error: "Unauthorized" })
+
+    req.user = user
+    next()
+  } catch (error) {
+    if (error instanceof TokenExpiredError) {
+      return res.status(401).json({ error: "Token has expired" })
+    } else if (error instanceof JsonWebTokenError) {
+      return res.status(403).json({ error: "Invalid token" })
+    }
+    return res.status(500).json({ error: "Internal server error" })
   }
 }
